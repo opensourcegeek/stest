b5c371f0b640d2ab69bd19e5c202050ddd035175
opensourcegeek <praveen.kumar@nomadrail.com>
2017-01-11 17:04:04 +0000

    Pointing to forked version of cross

--- ci/install.sh
+++ ci/install.sh
@@ -16,7 +16,7 @@ main() {
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
-           --git japaric/cross \
+           --git opensourcegeek/cross \
            --tag v0.1.4 \
            --target $target
 }
